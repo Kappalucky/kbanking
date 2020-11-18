4547d96236e1413412d43d1e831123b76377533f
Angelique John <angeliquejohn@angeliqsmacbook.lan>
2020-11-18 04:23:02 -0500

    Added greetings section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="home">
-    <!--<TheGreeting />-->
-    <TheOverview />
+    <TheGreeting />
+    <!--<TheOverview />-->
     <!--<Transaction />-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-//import TheGreeting from "@/components/TheGreeting.vue";
-import TheOverview from "@/components/TheOverview.vue";
+import TheGreeting from "@/components/TheGreeting.vue";
+//import TheOverview from "@/components/TheOverview.vue";
 //import Transaction from "@/components/Transaction.vue";
 
 export default {
   name: "Home",
   components: {
-    //TheGreeting,
-    TheOverview
+    TheGreeting
+    //TheOverview
     //Transaction
   }
 };
